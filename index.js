9ee91bcbbc0daa79a9d502a34f72317da47d757d
GSCrawley <gscrawley@gmail.com>
2020-11-14 16:55:34 -0800

    month working

--- index.js
+++ index.js
@@ -21,57 +21,18 @@ class DL {
     }
 
   get month() {
-    this.monthMap = [{1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}]
-    const month = this.monthMap[this.date.getMonth()];
-    return month;
+    const monthIn = this.date.getMonth();
+    return this.months[monthIn]
     }
-  } 
+   
+    
+   
+  }
+
     const d = new DL(1973,8,15)
     console.log(d.year)
     console.log(d.shortYear) 
-    //     shortMonth() {
-    //         const month = this.month();
-    //         return month.slice(0, 3);
-    //         }
-        
-    //     day() {
-    //         return this.date.getUTCDate();
-    //         }
-        
-    //     longDay() {
-    //         const day = this.day();
-            
-    //         if (day < 10) {
-    //             return `0${day}`;
-    //         }
-            
-    //         return String(day);
-    //         }    
-        
-    //     hours() {
-    //         return this.date.getHours();
-    //         }
-        
-    //     longHours() {
-    //         const hours = this.hours();
-            
-    //         if (hours < 10) {
-    //             return `0${hours}`;
-    //         }
-            
-    //         return String(hours);
-    //         }
-    
-    //     minutes() {
-    //         return this.date.getMinutes();
-    //         }
-                
-    //     }
-        
-    // const d = new D('8/15/1973')
-    // const e = new D(1975, 1, 29)
-    
-
+    console.log(d.month)
     // console.log(d)
     // console.log(d.year)
     // console.log(e)
