be7d4bf0f7c0a6ca6cede63d3205b7e30a108087
GSCrawley <gscrawley@gmail.com>
2020-11-15 22:58:19 -0800

    got some help with the math and everything now works except short months for some reason

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// challenge 1
+const { format } = require("path");
 
 class DL {
   constructor(...args) {
@@ -25,10 +25,10 @@ class DL {
     return this.months[monthIn]
     }
   
-  // get shortMonth() {
-  //   const monthIn = this.date.getMonth().toString.slice(3)
-  //   return this.shortMonth[monthIn]
-  // } this aint workin
+  // get monthShrt() {
+  //   const monthIndex = this.date.getMonth() //get the month index
+  //   return this.mnthShrt[monthIndex]
+  // }
 
   get day() {
       return this.date.getDate()
@@ -45,13 +45,111 @@ class DL {
   get secs() {
       return this.date.getSeconds()
     }
+  
+
+  format(str=null) {
+    const unmask = {
+      'Y': this.year,
+      'y': this.shortYear,
+      'M': this.month,
+      'D': this.day,
+      'H': this.hours,
+      'm': this.mins,
+      'S': this.secs
   }
+
+  if (str == null) {
+    return `${unmask['Y']} ${unmask['M']} ${unmask['D']}`
+  }
+
+  let result = ''
+  //loop over the string and 
+  for (let i=0; i < str.length; i++) {
+    let ch = str[i]
+    if (ch in unmask) {
+      result += unmask[ch]
+    }
+    else {
+      result += ch
+        //else append the ch
+    }
+  }
+  return result;
+}
+
+when() {
+  const curr = new Date()
+  let diff = curr.getTime() - this.date.getTime()
+  //check if there is no difference
+  if (diff == 0) {
+    return 'now'
+  }
+  //check if there is a seconds difference
+  //calculate seconds
+  diff /= 1000
+  if (Math.abs(diff) <= 44) {
+    return `a few seconds ` +  (diff < 0 ? `from now`: `ago`)
+  }
+
+  if (Math.abs(diff) == 44) {
+    return `${Math.abs(diff).toFixed(0)} seconds ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  if (Math.abs(diff) >= 44 && Math.abs(diff) <= 89) {
+    return `a minute ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  // calculate minute
+  diff /= 60
+  if (Math.abs(diff) >= 1.5 && Math.abs(diff) <= 44) {
+    return `${Math.abs(diff).toFixed(0)} minutes ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  //an hour
+  if (Math.abs(diff) > 44 && Math.abs(diff) <= 89) {
+    return `an hour ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  //calculate hour
+  diff /= 60
+  if (Math.abs(diff) > 1.5 && Math.abs(diff) <= 21) {
+    return `${Math.abs(diff).toFixed(0)} hours ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  // a day
+  if (Math.abs(diff) > 21 && Math.abs(diff) <= 35) {
+    return `a day ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  //calculate days
+  diff /= 24
+  if (Math.abs(diff) > 1.6 && Math.abs(diff) <= 25) {
+    return `${Math.abs(diff).toFixed(0)} days ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  // a month
+  if (Math.abs(diff) > 25 && Math.abs(diff) < 45) {
+    return `a month ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  //calculate months
+  diff /= 30
+  if (Math.abs(diff) >= 1.4 && Math.abs(diff) <= 10.5) {
+    return `${Math.abs(diff).toFixed(0)} months ` + (diff < 0 ? `from now`: `ago`)
+  }
+
+  // year
+  diff /= 12
+  return `${Math.abs(diff).toFixed(0)} year` + (Math.abs(diff) < 1.5 ? ' ': 's ') + (diff < 0 ? `from now`: `ago`)
+}
+}
+
 
     const d = new DL(1973,8,15,11,25,17)
     console.log(d.year)
     console.log(d.shortYear) 
     console.log(d.month)
-    console.log(d.shortMonth)
+    console.log(d.monthShrt)
     console.log(d.day)
     console.log(d.hours)
     console.log(d.mins)
