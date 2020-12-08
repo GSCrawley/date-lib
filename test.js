79cecbd5afcdc62310b08f0ea05fc23114cfa22d
GSCrawley <gscrawley@gmail.com>
2020-12-08 15:24:47 -0800

    all tests a passin

--- test.js
+++ test.js
@@ -1,54 +1,57 @@
-require
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-expressions
+require;
 
 const DL = require('./umd/date-lib.js');
 
 const bday = new DL(1973, 7, 7, 3, 9, 5);
 
-const date_test = bday;
+const dateTest = bday;
+
 test('year', () => {
-    expect(date_test.year.toBe = '1973');
-}),
+  expect(dateTest.year.toBe = '1973');
+});
 
 test('shortYear', () => {
-  expect(date_test.shortYear.toBe = '73');
-}),
+  expect(dateTest.shortYear.toBe = '73');
+});
 
 test('month', () => {
-  expect(date_test.month.toBe = 'August');
-}),
+  expect(dateTest.month.toBe = 'August');
+});
 
 test('monthShrt', () => {
-  expect(date_test.monthShrt.toBe = 'Aug');
-}),
+  expect(dateTest.monthShrt.toBe = 'Aug');
+});
 
 test('day', () => {
-  expect(date_test.day.toBe = '7');
-}),
+  expect(dateTest.day.toBe = '7');
+});
 
 test('hours', () => {
-  expect(date_test.hours.toBe = '3');
-}),
+  expect(dateTest.hours.toBe = '3');
+});
 
 test('mins', () => {
-  expect(date_test.mins.toBe = '9');
-}),
+  expect(dateTest.mins.toBe = '9');
+});
 
 test('secs', () => {
-  expect(date_test.secs.toBe = '5');
-}),
+  expect(dateTest.secs.toBe = '5');
+});
 
 test('dayPad', () => {
-  expect(date_test.dayPad.toBe = '07');
-}),
+  expect(dateTest.dayPad.toBe = '07');
+});
 
 test('hoursPad', () => {
-  expect(date_test.dayPad.toBe = '03');
-}),
+  expect(dateTest.dayPad.toBe = '03');
+});
 
 test('minsPad', () => {
-  expect(date_test.minsPad.toBe = '09');
-}),
+  expect(dateTest.minsPad.toBe = '09');
+});
 
 test('secsPad', () => {
-  expect(date_test.dayPad.toBe = '05');
+  expect(dateTest.dayPad.toBe = '05');
 });
